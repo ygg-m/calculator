40875465c137f2adc310f361b5f77401c9b857a9
Ygor Goulart <ygorgm95@gmail.com>
2022-12-22 15:11:56 -0300

    feat get elements i njs

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+// elements
+const result = document.getElementById("result");
+const numbers = document.getElementsByClassName("number");
+const operators = document.getElementsByClassName("operator");
+const clear = document.getElementsByClassName("clear")[0];
+const equal = document.getElementsByClassName("equal")[0];
