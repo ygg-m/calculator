83e9c594344cc3d395a18b36639bd326af743820
Ygor Goulart <ygorgm95@gmail.com>
2022-12-22 15:21:44 -0300

    feat add click to numbers

--- Assets/script.js
+++ Assets/script.js
@@ -4,3 +4,19 @@ const numbers = document.getElementsByClassName("number");
 const operators = document.getElementsByClassName("operator");
 const clear = document.getElementsByClassName("clear")[0];
 const equal = document.getElementsByClassName("equal")[0];
+
+// initial values
+let currentNumber = "";
+let previousNumber = "";
+let operator = "";
+
+// add event listeners to numbers
+for (let number of numbers) {
+  number.addEventListener("click", (e) => {
+    // append clicked number
+    currentNumber += e.target.value;
+
+    // update result
+    result.value = currentNumber;
+  });
+}
